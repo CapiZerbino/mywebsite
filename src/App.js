1f9a2a1a8288ef3c44aacc0fe197315e0b00cfd6
CapiZerbino <tvt.nghia3001@gmail.com>
2021-12-31 16:45:06 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/UI/Footer";
+import Navigation from "./components/UI/Navigation";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="w-full h-full bg-neutral-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Navigation></Navigation>
         <Router>
           <Routes>
             <Route exact path="/" element={<HomePage />}></Route>
           </Routes>
         </Router>
+        <Footer></Footer>
       </div>
     </div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import Portfolio from '../components/HomePage/Portfolio'
-import Footer from '../components/UI/Footer'
-import Navigation from '../components/UI/Navigation'
-import Features from './../components/HomePage/Features'
-import HeroSection from './../components/HomePage/HeroSection'
+import React from "react";
+import Portfolio from "../components/HomePage/Portfolio";
+import Features from "./../components/HomePage/Features";
+import HeroSection from "./../components/HomePage/HeroSection";
 
 function HomePage() {
-    return (
-        <>
-           <Navigation></Navigation>
-           <HeroSection></HeroSection>
-           <Features></Features>
-           <Portfolio></Portfolio>
-           <Footer></Footer>
-        </>
-    )
+  return (
+    <>
+      <HeroSection></HeroSection>
+      <Features></Features>
+      <Portfolio></Portfolio>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
