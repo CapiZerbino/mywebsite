57546e7331fd6d87475684024581d0577e791d4d
CapiZerbino <tvt.nghia3001@gmail.com>
2021-12-31 16:58:38 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/UI/Footer";
 import Navigation from "./components/UI/Navigation";
 import HomePage from "./pages/HomePage";
@@ -7,17 +7,13 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <BrowserRouter>
-    <div className="w-full h-full bg-neutral-900">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Navigation></Navigation>
-        
-          <Routes>
-            <Route exact path="/" element={<HomePage />}></Route>
-          </Routes>
-        
-        <Footer></Footer>
+      <div className="w-full h-full bg-neutral-900">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Navigation></Navigation>
+          <HomePage />
+          <Footer></Footer>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 }
